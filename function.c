bad5a8c611cd9d277c9901fb71820f63ded8e2da
Carlos Valle Torres <carlos.valle@deimos-space.com>
2017-10-04 15:04:39 +0200

    Create a new function

--- function.c
+++ function.c
@@ -1,3 +1,18 @@
 /**
  * Include some comments here
  */
+
+#include <stdio.h>
+
+void function();
+
+void function()
+{
+  printf("This function has been created for RAMA_Y\n");
+}
+
+int main()
+{
+
+  function();
+}
